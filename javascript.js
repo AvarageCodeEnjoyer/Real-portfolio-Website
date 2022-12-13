58d6f282bffe9c474b745aad3faffeed344910e4
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2022-12-13 16:37:05 -0700

    Made REPO

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var navName = document.getElementsByClassName('navName')
+// TODO make full name appear on hover
+/* function fullName(){ 
+  let FullName = navName.innerHTML;
+  FullName = "Niko Drossos"
+  console.log("hello")
+} */
