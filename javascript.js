fd237a20dbfdba358390c6ba8f7d8fdbf74e2307
niko-drossos <8she682gsyhaas@protonmail.com>
2024-05-21 13:21:22 -0700

    Updated profile

--- javascript.js
+++
@@ -1,7 +0,0 @@
-var navName = document.getElementsByClassName('navName')
-// TODO make full name appear on hover
-/* function fullName(){ 
-  let FullName = navName.innerHTML;
-  FullName = "Niko Drossos"
-  console.log("hello")
-} */
